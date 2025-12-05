b604043ebb9f1bb7ca51a6b7382a34c06afd4f20
bmnazmussakib <nazmussakib161@gmail.com>
2025-12-05 22:33:42 +0600

    update

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -58,6 +58,10 @@
                         class="border border-gray-400 py-1.5 px-3 w-full focus:outline-none focus:ring-1 focus:ring-[#27ad4c]" />
                     <button class="absolute right-0 top-0 h-full px-2 text-white bg-[#27ad4c]">
                         <!-- SearchIcon -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                        </svg>
+
                     </button>
                 </div>
 
@@ -96,7 +100,7 @@
                     Contact
                 </a>
                 {{-- <a href="{{ route('details',10) }}" wire:navigate class="pb-2 hover:text-[#27ad4c]">
-                    Product Details
+                Product Details
                 </a> --}}
 
                 {{-- <a href="#" class="pb-2 hover:text-[#27ad4c]">Software</a>
