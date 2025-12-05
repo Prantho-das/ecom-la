2fe21290f28153dbbf969a4b6bdc97f670d31bec
bmnazmussakib <nazmussakib161@gmail.com>
2025-12-05 22:53:32 +0600

    update

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -50,9 +50,22 @@
                     <div class="mt-8">
                         <a
                             href="#"
-                            class="inline-block bg-[#27ad4c] text-white font-semibold py-3 px-8 rounded-md hover:bg-green-700 transition-colors">
+                            class="inline-block bg-[#27ad4c] text-white font-semibold py-3 px-8 rounded-md hover:bg-green-700 transition-colors"
+                            >
                             Contact Sales
                         </a>
+                        <!-- Open the modal using ID.showModal() method -->
+                        <button class="btn" onclick="my_modal_2.showModal()">open modal</button>
+                        <dialog id="my_modal_2" class="modal">
+                            <div class="modal-box">
+                                <h3 class="text-lg font-bold">Hello!</h3>
+                                <p class="py-4">Press ESC key or click outside to close</p>
+                            </div>
+                            <form method="dialog" class="modal-backdrop">
+                                <button>close</button>
+                            </form>
+                        </dialog>
+
                     </div>
                 </div>
 
