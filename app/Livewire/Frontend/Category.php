c12c4f5b82ee767a8729689407959f89e2200ab5
Prantho-das <pranthokumardas786@gmail.com>
2025-12-07 02:57:34 +0600

    contact

--- app/Livewire/Frontend/Category.php
+++ app/Livewire/Frontend/Category.php
@@ -11,9 +11,9 @@ class Category extends Component
     public $category;
     public $category_products=[];
     public function mount(){
-        if(request()->filled('category_slug'),function(){
+        if(request()->filled('category_slug')){
             $this->category=Category::where('slug',request()->category_slug)->first();
-        })
+        }
         $this->category_products = Product::with(['images'])
             ->whereHas('categories', function ($q) {
                 if($this->category){
