a2f5672881a73cbd138be9fe69c51ee1cec4224d
Prantho-das <pranthokumardas786@gmail.com>
2025-12-07 18:31:20 +0600

    category page problem

--- resources/views/livewire/frontend/category.blade.php
+++ resources/views/livewire/frontend/category.blade.php
@@ -108,7 +108,7 @@
                     <div class="flex justify-end items-center gap-3 mb-6 pb-4 border-b border-gray-200">
                         <div class="flex items-center space-x-2">
                     
-                            <button  wire:click="$set('showMode', 'grid')" class="p-1 text-2xl {{ $showMode!='grid'?'text-green-600':'text-black' }}">
+                            <button  wire:click="$set('showMode', 'grid')" class="p-1 text-2xl {{ $showMode=='grid'?'text-green-600':'text-black' }}">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                     class="size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -116,7 +116,7 @@
                                 </svg>                                  
                             </button>
                 
-                            <button wire:click="$set('showMode', 'list')"  class="p-1 text-2xl {{ $showMode=='grid'?'text-green-600':'text-black' }}">
+                            <button wire:click="$set('showMode', 'list')"  class="p-1 text-2xl {{ $showMode=='list'?'text-green-600':'text-black' }}">
                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                     class="size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
