b252b551d1f6b14d38ed8b42b2281cddd8c8ddd9
bmnazmussakib <nazmussakib161@gmail.com>
2025-12-08 18:50:15 +0600

    upate

--- resources/views/livewire/frontend/details.blade.php
+++ resources/views/livewire/frontend/details.blade.php
@@ -63,7 +63,7 @@
 
             <!-- Right Column - Details -->
             <div>
-                <div class="w-32 mb-2">
+                <div class="w-10 mb-2">
                     <img src="{{ $product->brand && $product->brand->logo_path ? asset('storage/' . $product->brand->logo_path) : 'https://placehold.co/600x400?text=' . $product->name }}"
                         alt="{{ $product->brand?->name }}">
                 </div>
@@ -87,13 +87,15 @@
                         outlets
                     </li>
                     <li class="flex items-start "><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-green-600 mr-2">
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            class="size-4 text-green-600 mr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                         </svg>
                         3 USB-A and 1 USB-C
                         PD60W</li>
                     <li class="flex items-start "><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-green-600 mr-2">
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            class="size-4 text-green-600 mr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                         </svg>
                         Recharge via wall,
@@ -137,8 +139,13 @@
                         @foreach ($product->pdf_files as $pdf_file)
                             <a href="{{ asset('storage/' . $pdf_file) }}" target="_blank"
                                 class="flex items-center text-gray-700 hover:text-green-600 group">
-                                <span class="mr-2 text-red-700">
-                                    <x-bi-file-pdf-fill />
+                                <span class="mr-2 ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5 text-red-700">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                    </svg>
+
                                 </span> {{ basename($pdf_file) }}
                             </a>
                         @endforeach
