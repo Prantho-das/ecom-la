7c3b2e6123a97881d41ef0f5b59ad9920236d9ae
Prantho-das <pranthokumardas786@gmail.com>
2025-12-09 15:48:16 +0600

    category

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     // App\Providers\Filament\AdminPanelProvider::class,
-    // App\Providers\Filament\DarkAdminPanelProvider::class,
+    App\Providers\Filament\DarkAdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
