76a8bceb96d0d2cd7b49c82560509906d382c632
Prantho-das <pranthokumardas786@gmail.com>
2026-02-21 00:16:43 +0600

    currency

--- app/Filament/DarkAdmin/Resources/Quotations/Pages/QuotationBuilder.php
+++ app/Filament/DarkAdmin/Resources/Quotations/Pages/QuotationBuilder.php
@@ -140,7 +140,7 @@ class QuotationBuilder extends Page
             'product_id' => '',
             'selected_incoterm' => 'DDP',
             'name' => '',
-            'quantity' => 1,
+            'quantity' => 0,
             'uom' => 'UNIT',
             'unit_product_price' => 0,
             'currency' => \App\Models\Currency::where('is_base', true)->first()?->code ?? 'USD',
